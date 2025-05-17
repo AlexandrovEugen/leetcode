f24dac00043016bcf4f67376dc24b90cf599831f
e.aleksandrov <e.aleksandrov@proxyimity.io>
2025-05-17 14:11:45 +0200

    refactored the solved problems to encapsulate the private methods

--- src/subsets/Solution.py
+++ src/subsets/Solution.py
@@ -1,46 +1,41 @@
-from collections import deque
-
-
-def swap_chars(i, j, word):
-    sw = list(word)
-    sw[i], sw[j] = sw[j], sw[i]
-    return ''.join(sw)
-
-
-def backtrack_0(word, c_i, result):
-    if len(word) - 1 == c_i:
-        result.append(word)
-        return
-
-    for i in range(c_i, len(word)):
-        swap_str = swap_chars(c_i, i, word)
-        backtrack_0(swap_str, c_i + 1, result)
-
 def permute_word(word):
     result = []
-    backtrack_0(word, 0, result)
+
+    def swap_chars(i, j, string):
+        sw = list(string)
+        sw[i], sw[j] = sw[j], sw[i]
+        return ''.join(sw)
+
+    def backtrack_0(string, c_i):
+        if len(string) - 1 == c_i:
+            result.append(string)
+            return
+
+        for i in range(c_i, len(string)):
+            swap_str = swap_chars(c_i, i, string)
+            backtrack_0(swap_str, c_i + 1)
+
+    backtrack_0(word, 0)
     return result
-
-
-def backtrack(n:int, l:int, r:int, output, result:list[str]):
-    if l >= n and r >=n:
-        result.append(''.join(output))
-
-    if l < n:
-        output.append('(')
-        backtrack(n, l + 1, r, output, result)
-        output.pop()
-
-    if r < l:
-        output.append(')')
-        backtrack(n, l, r + 1, output, result)
-        output.pop()
 
 
 def generate_combinations(n):
     result = []
     output = []
-    backtrack(n, 0, 0, output, result)
+
+    def backtrack(l: int, r: int):
+        if l >= n and r >= n:
+            result.append(''.join(output))
+
+        if l < n:
+            output.append('(')
+            backtrack(l + 1, r)
+            output.pop()
+
+        if r < l:
+            output.append(')')
+            backtrack(l, r + 1)
+            output.pop()
+
+    backtrack(0, 0)
     return result
-
-
--- test/subsets/Solution_test.py
+++ test/subsets/Solution_test.py
@@ -1,32 +1,37 @@
 import unittest
-from src.subsets.Solution import permute_word, generate_combinations
+from src.subsets.Solution import permute_word, generate_combinations, letter_combinations
+
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, False)  # add assertion here
 
     def test_permuted_word(self):
-
         word = "abcd"
 
         result = permute_word(word)
 
-        expected_result = ["abcd","abdc","acbd","acdb","adbc","adcb","bacd","badc","bcad","bcda","bdac","bdca","cabd","cadb","cbad","cbda","cdab","cdba","dabc","dacb","dbac","dbca","dcab","dcba"]
+        expected_result = ["abcd", "abdc", "acbd", "acdb", "adbc", "adcb", "bacd", "badc", "bcad", "bcda", "bdac",
+                           "bdca", "cabd", "cadb", "cbad", "cbda", "cdab", "cdba", "dabc", "dacb", "dbac", "dbca",
+                           "dcab", "dcba"]
 
         self.assertSequenceEqual(sorted(result), sorted(expected_result))
 
     def test_generate_combinations(self):
-
         n = 3
 
-        expected = ["((()))","(()())","(())()","()(())","()()()"]
+        expected = ["((()))", "(()())", "(())()", "()(())", "()()()"]
 
         result = generate_combinations(n)
 
         self.assertSequenceEqual(sorted(expected), sorted(result))
 
+    def test_letter_combinations(self):
+        result = letter_combinations("2")
 
+        expected = ["a", "b", "c"]
 
+        self.assertSequenceEqual(sorted(result), sorted(expected))
 
 if __name__ == '__main__':
     unittest.main()
