5b2b66f55e941874943be74d9e8bc8d2d2e5e1c5
Evgenii Aleksandrov <evgeniy.alexandrov1994@gmail.com>
2025-06-17 10:59:20 +0200

    sort and search patter is done

--- src/sort_search/Solution.py
+++ src/sort_search/Solution.py
@@ -5,9 +5,41 @@ def find_best_value(arr, target):
 
     for i, num in enumerate(arr):
         if remaining_target <= num * (n - i):
-            replacement_value = remaining_target/ (n - i)
+            replacement_value = remaining_target / (n - i)
             if replacement_value - int(replacement_value) == 0.5:
                 return int(replacement_value)
             return round(replacement_value)
-        remaining_target -=num
+        remaining_target -= num
     return arr[-1]
+
+
+def contains_nearby_duplicates(nums: list[int], k: int) -> bool:
+    seen = set()
+
+    for i in range(len(nums)):
+        if nums[i] in seen:
+            return True
+
+        seen.add(nums[i])
+
+        if len(seen) > k:
+            seen.remove(nums[i - k])
+
+    return False
+
+
+def max_count(banned: list[int], n: int, max_sum: int) -> int:
+    banned.sort()
+    banned_idx = 0
+    count = 0
+
+    for i in range(1, n + 1):
+        if banned_idx < len(banned) and banned[banned_idx] == i:
+            while banned_idx < len(banned) and banned[banned_idx] == i:
+                banned_idx += 1
+        else:
+            max_sum -= i
+            if max_sum < 0:
+                return count
+            count += 1
+    return count
--- test/sort_search/Solution_test.py
+++ test/sort_search/Solution_test.py
@@ -2,13 +2,34 @@ import unittest
 
 from sort_search.Solution import *
 
+
 class MyTestCase(unittest.TestCase):
 
     def test_find_best_value(self):
-        res=find_best_value([2,3,5,10] , 13)
+        res = find_best_value([2, 3, 5, 10], 13)
 
         self.assertEqual(res, 4)
 
+
+
+    def test_contains_nearby_duplicates(self):
+        nums = [7, 8, 6, 7, 9]
+
+        k = 3
+
+        res = contains_nearby_duplicates(nums, k)
+
+        self.assertEqual(res, True)
+
+
+    def test_max_count(self):
+        nums = [10, 20, 30]
+        n = 50
+        max_sum = 6
+
+        res = max_count(nums, n, max_sum)
+
+        self.assertEqual(res, 3)
 
 if __name__ == '__main__':
     unittest.main()
